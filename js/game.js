72c9ddc677337db6cc71b92c2f08d042720b079f
agargara <dilots@gmail.com>
2020-08-02 11:14:01 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,8 +1,6 @@
 /* TODO
   HIGH
   refactor code:
-    - store ordered purchased node list in Character
-      - this will allow save/load of builds, as well as separate builds per character
     - when drawing nodes check current character to decide which nodes are active
   test if performance suffers with 1000 nodes
 
